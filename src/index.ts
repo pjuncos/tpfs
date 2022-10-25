e9a4aba8b2d56a9f176f26b85a5baf8ca2ae29ef
pjuncos <pablojuncos@wengan.com.ar>
2022-10-25 14:29:43 -0300

    Update api version

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const port = process.env.SERVER_PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use("/api/v1", routes);
 
 app.listen(port, () => {
   console.log(`🚀 Server started at http://localhost:${port}`);
