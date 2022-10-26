6ec4f71a536603576cca9c8122fdfc44bf01b7de
pjuncos <pablojuncos@wengan.com.ar>
2022-10-26 12:21:44 -0300

    Add the possibility to check roles for routes

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,26 +2,35 @@ import { verify } from "jsonwebtoken";
 import { Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import { CustomRequest } from "./request";
-import User from "../models/user";
+import User, { ROLES } from "../models/user";
 
 dotenv.config();
 
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret_key";
 
-const verifyAuth = (req: CustomRequest, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
+const verifyAuth = (roles?: [ROLES]) => {
+  return (req: CustomRequest, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Authorization token is missing" });
+    }
 
-  if (!token) {
-    return res.status(401).json({ message: "Authorization token is missing" });
-  }
+    try {
+      const payload = verify(token, TOKEN_SECRET);
+      const user = new User(payload);
 
-  try {
-    const payload = verify(token, TOKEN_SECRET);
-    req.user = new User(payload);
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
-  }
+      if (roles && !roles.includes(user.role)) {
+        return res.status(403).json({ message: "Unauthorized access" });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  };
 };
 
 export { verifyAuth };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyAuth } from "../middlewares/auth";
 import * as UserController from "../controllers/user";
+import { ROLES } from "../models/user";
 
 const router = Router();
 
-router.get("/", [verifyAuth], UserController.getAllUsers);
+router.get("/", verifyAuth([ROLES.ADMIN]), UserController.getAllUsers);
 router.post("/", UserController.addUser);
-router.put("/:id", [verifyAuth], UserController.editUser);
-router.get("/:id", [verifyAuth], UserController.getUser);
+router.put("/:id", verifyAuth(), UserController.editUser);
+router.get("/:id", verifyAuth(), UserController.getUser);
 
 export default router;
